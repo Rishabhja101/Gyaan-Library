54848773e0ecce852946787857d7a66d910614cf
kashish2001 <33819630+kashish2001@users.noreply.github.com>
2018-02-23 17:07:57 -0800

    kaaaaaaasheeeeeesh

---
+++ GyaanLibrary/app/src/androidTest/java/com/example/risha/gyaanlibrary/Book.java
@@ -0,0 +1,10 @@
+package com.example.risha.gyaanlibrary;
+
+public class Book {
+
+    // title, author, summary
+    // reviews/reply to the reviews
+    // preview
+    // publication info
+
+}
--- GyaanLibrary/app/src/main/java/com/example/risha/gyaanlibrary/LoginActivity.java
+++ GyaanLibrary/app/src/main/java/com/example/risha/gyaanlibrary/LoginActivity.java
@@ -1,45 +1,34 @@
 package com.example.risha.gyaanlibrary;
 
-import android.support.annotation.NonNull;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.EditText;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
-
 public class LoginActivity extends AppCompatActivity {
-    private FirebaseAuth mAuth;
-    private String username;
-    private String password;
+//    private FirebaseAuth mAuth;
+//    private String username;
+//    private String password;
+//    Bundle bundle = new Bundle();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        mAuth = FirebaseAuth.getInstance();
+//        mAuth = FirebaseAuth.getInstance();
+        Toast.makeText(this, "LoginActivity", Toast.LENGTH_SHORT).show();
+        // bundle = getIntent().getBundleExtra("pass");
     }
-
-    private void GetCredentials(){
-        EditText usernameText = (EditText)findViewById(R.id.username);
+/*
+    private void GetCredentials() {
+        EditText usernameText = (EditText) findViewById(R.id.username);
         username = usernameText.getText().toString();
-        EditText passwordText = (EditText)findViewById(R.id.password);
+        EditText passwordText = (EditText) findViewById(R.id.password);
         password = passwordText.getText().toString();
     }
 
     int userIndex;
 
-    public void AttemptLogin(View view){
+    public void AttemptLogin(View view) {
         GetCredentials();
         FirebaseDatabase database = FirebaseDatabase.getInstance();
         DatabaseReference myRef = database.getReference("username");
@@ -48,7 +37,7 @@ public class LoginActivity extends AppCompatActivity {
             public void onDataChange(DataSnapshot dataSnapshot) {
                 userIndex = 0;
                 String usernames[] = dataSnapshot.getValue().toString().split(",");
-                while (userIndex < usernames.length && !usernames[userIndex].equals(username)){
+                while (userIndex < usernames.length && !usernames[userIndex].equals(username)) {
                     userIndex++;
                 }
             }
@@ -63,10 +52,9 @@ public class LoginActivity extends AppCompatActivity {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
                 String passwords[] = dataSnapshot.getValue().toString().split(",");
-                if (userIndex < passwords.length && passwords[userIndex].equals(password)){
+                if (userIndex < passwords.length && passwords[userIndex].equals(password)) {
                     Toast.makeText(LoginActivity.this, "success", Toast.LENGTH_SHORT).show();
-                }
-                else{
+//                } else {
                     Toast.makeText(LoginActivity.this, "fail", Toast.LENGTH_SHORT).show();
                 }
             }
@@ -78,10 +66,9 @@ public class LoginActivity extends AppCompatActivity {
         });
     }
 
-    public void CreateUser(View view){
+    public void CreateUser(View view) {
         GetCredentials();
         Toast.makeText(LoginActivity.this, "asdfsdafsdafadsfghtyrkjyutmkyuilyuilkuiykyuik.", Toast.LENGTH_SHORT).show();
     }
-
-
+*/
 }
